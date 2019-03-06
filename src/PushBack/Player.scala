fdf0bd9de1bd2f939313c0498e0c5533f6db24d5
Connie Liew <cliew@buffalo.edu>
2019-03-05 20:12:54 -0500

    the new html file

--- src/PushBack/Player.scala
+++ src/PushBack/Player.scala
@@ -1,5 +1,11 @@
 package PushBack
 
 object Player {
+  def keyPressed(keyCode: KeyCode): Unit = { keyCode.getName match {
+    case "Up" => player.translateY.value -= playerSpeed
+    case "Left" => player.translateX.value -= playerSpeed
+    case "S" => player.translateY.value += playerSpeed
+    case "D" => player.translateX.value += playerSpeed
+    case _ => println(keyCode.getName + " pressed with no action")
 
 }
